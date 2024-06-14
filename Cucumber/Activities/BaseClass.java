4351a12b6df68009de3956a00ae1642de553dcbe
Thanuja-140 <Valluri.thanuja@ibm.com>
2024-06-15 01:12:53 +0530

    Add files via upload

---
+++ Cucumber/Activities/BaseClass.java
@@ -0,0 +1,18 @@
+package stepDefinitions;
+
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseClass {
+//Create a new instance of the Firefox driver
+    static WebDriver driver;
+    static WebDriverWait wait;
+
+}
+
